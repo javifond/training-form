8957a6e9030abdcd94ad81c4eb0017ef4c831ca8
Javier Betancourth <javier.betancourth@gig.com>
2020-03-15 17:07:21 +0100

    TF- Add test action into a component

--- src/components/questionOne/questionOne.js
+++ src/components/questionOne/questionOne.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { simpleAction } from '../../actions/simpleAction';
 
-function QuestionOne() {
-    return (
-        <h2>Question One</h2>
-    )
+const mapDispatchToProps = {
+    simpleAction,
 }
 
-export default QuestionOne;
+const mapStateToProps = state => ({
+    ...state
+   })
+
+class QuestionOne extends Component {
+    simpleAction = (e) => {
+        this.props.simpleAction(e.target);
+    }
+
+    render() {
+        return (
+            <section>
+                <h2>Question One</h2>
+                <button onClick={(e) => this.simpleAction(e)}>Click me!</button>
+                { console.log(this.props) }
+            </section> 
+        )
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuestionOne);
