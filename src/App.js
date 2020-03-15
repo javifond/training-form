67e6638130eba95c15c5c7ded4f607a720fa754e
Javier Betancourth <javier.betancourth@gig.com>
2020-03-15 11:43:53 +0100

    TF - Update assets and app description

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Corona Virus Checker - Tell us your simptoms!
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Continue
         </a>
       </header>
     </div>
