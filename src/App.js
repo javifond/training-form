c139410a0f3989239e89bc4cc925f8206c5c454f
Javier Betancourth <javier.betancourth@gig.com>
2020-03-16 09:34:45 +0100

    TF - Add question two to the router and routing from Q1 to Q2 on continue action

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/home/home';
 import QuestionOne from './components/questionOne/questionOne';
+import QuestionTwo from './components/questionTwo/questionTwo';
 
 function App() {
-	return (
-		<Router>
-			<Switch>
-					<Route exact path="/">
-						<Home />
-					</Route>
-					<Route path="/question-one">
-						<QuestionOne />
-					</Route>
-				</Switch>
-		</Router>
-	);
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/question-one">
+                    <QuestionOne />
+                </Route>
+                <Route path="/question-two">
+                    <QuestionTwo />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
