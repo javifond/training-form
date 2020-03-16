a5f87c782a918f8645dc029ff5a5ef1257bc64a8
Javier Betancourth <javier.betancourth@gig.com>
2020-03-16 10:08:50 +0100

    TF - Add router and style layout using composes

--- src/components/questionTwo/questionTwo.js
+++ src/components/questionTwo/questionTwo.js
@@ -1,36 +1,43 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
 import styles from './questionTwo.module.scss';
 
 class QuestionTwo extends Component {
+    state = {
+        isValid: false,
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        this.props.history.push('/');
+    };
+
     render() {
         return (
             <section className={styles.root}>
-                <form  className={styles.content}>
-                    <p>
+                <form onSubmit={this.handleSubmit} className={styles.container}>
+                    <p className={styles.title}>
                         Are you experiencing runny nose, sore throat or/and
                         cough?
                     </p>
-                    <div>
-                        <input
-                            type="radio"
-                            id="yep"
-                            name="yes"
-                        />
+                    <div className={styles.radioContainer}>
+                        <input hidden type="radio" id="yep" name="yes" />
                         <label htmlFor="yep">Yes</label>
 
-                        <input
-                            type="radio"
-                            id="nope"
-                            name="no"
-                        />
+                        <input hidden type="radio" id="nope" name="no" />
                         <label htmlFor="nope">No</label>
                     </div>
-                    <button type="submit">Continue</button>
+                    {this.state.isValid && (
+                        <button className={styles.button} type="submit">
+                            Continue
+                        </button>
+                    )}
                 </form>
             </section>
         );
     }
 }
 
-export default QuestionTwo;
+export default withRouter(QuestionTwo);
