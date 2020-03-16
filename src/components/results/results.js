2fe065b6d22eec21992d58c1913681358c13c877
Javier Betancourth <javier.betancourth@gig.com>
2020-03-16 15:18:15 +0100

    TF: Use global state to determine the results

--- src/components/results/results.js
+++ src/components/results/results.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import styles from './results.module.scss';
+import { hasFever } from '../questionOne/questionOne.utils';
+
+const mapStateToProps = (state) => ({
+    ...state,
+});
 
 class Results extends Component {
+    state = {
+        mightHaveTheVirus: false,
+    };
+
+    componentDidMount() {
+        const { hasFever, hasWheeze } = this.props;
+
+        this.setState({
+            mightHaveTheVirus: hasFever && hasWheeze,
+        });
+    }
+
     handleClick = (e) => {
         e.preventDefault();
 
@@ -13,20 +31,36 @@ class Results extends Component {
     render() {
         return (
             <section className={styles.root}>
-                <article className={styles.container}>
-                    <p>Ouh! Your symptoms indecate that you <strong>might have </strong>Corona Virus.</p>
-                    <p>Please stay at home.</p>
-                    <p>And call the authorities.</p>
-                    <p>Do it for your safety and for those around you.</p>
-                </article>
+                {this.state.mightHaveTheVirus && (
+                    <article className={styles.container}>
+                        <p>
+                            Ouh! Your symptoms indecate that you{' '}
+                            <strong>might have </strong>Corona Virus.
+                        </p>
+                        <p>Please stay at home.</p>
+                        <p>And call the authorities.</p>
+                        <p>Do it for your safety and for those around you.</p>
+                    </article>
+                )}
 
-                <article className={styles.container}>
-                    <p>Uff! You <strong>might not have</strong> Corona Virus.</p>
-                    <p>But as a general recommendation, please stay at home.</p>
-                    <p>Let's be resposible and show solidarity to our comunity.</p>
-                    <p>Thanks a lot.</p>
-                </article>
-                
+                {!this.state.mightHaveTheVirus && (
+                    <article className={styles.container}>
+                        <p>
+                            Uff! You <strong>might not have</strong> Corona
+                            Virus.
+                        </p>
+                        <p>
+                            But as a general recommendation, please stay at
+                            home.
+                        </p>
+                        <p>
+                            Let's be resposible and show solidarity to our
+                            comunity.
+                        </p>
+                        <p>Thanks a lot.</p>
+                    </article>
+                )}
+
                 <button
                     onClick={this.handleClick}
                     className={styles.button}
@@ -39,4 +73,4 @@ class Results extends Component {
     }
 }
 
-export default withRouter(Results);
+export default connect(mapStateToProps, null)(withRouter(Results));
